94e369cdd6175d34879a1177806cdebb064c2270
affanbachdar <32550992+affanbachdar@users.noreply.github.com>
2023-07-30 11:29:53 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-qa4ptf-8qyvbt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-qa4ptf-favbcr)
---
+++ script.js
@@ -0,0 +1,9 @@
+//
+// Place any custom JS here
+//
+//open and close tab menu
+$('.nav-tabs-dropdown')
+    .on("click", "li:not('.active') a", function(event) {  $(this).closest('ul').removeClass("open");
+    })
+    .on("click", "li.active a", function(event) {        $(this).closest('ul').toggleClass("open");
+    });
